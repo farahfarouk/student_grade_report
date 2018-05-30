bdef2d232709e78ace2cfd5ea5e6f5c18283e631
Farah <30908196+ffarah93@users.noreply.github.com>
2018-05-30 09:29:46 -0700

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class Student
+    {
+
+        public string Name { get; set; }
+
+        public double Midtermgrade { get; set; }
+
+        public double Finalgrade { get; set; }
+
+
+    }
+}
